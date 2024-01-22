1b1dadd2e838ef6fd580d4c81d6a5923efd292e0
Partha Sarathi Praharaj <parthasarathipraharaj@Parthas-MacBook-Pro.local>
2024-01-22 23:52:54 +0530

    server image upload

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,16 @@
-import { UserButton } from "@clerk/nextjs";
+import InitialModal from "@/components/modals/initial-modal";
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton,currentUser,redirectToSignIn } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  // console.log("USER:",user);
+  
   return (
     <div>
       <UserButton afterSignOutUrl="/sign-in" />
+      <ModeToggle />
+      <InitialModal />
     </div>
   );
 }
